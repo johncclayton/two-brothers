5186c01706fb4f77baf0b2dc087f5f93e8ae3e65
John Clayton <john@effective-flow.ch>
2025-06-12 06:57:49 +0200

    Spell fix.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 const isGitHubPages = process.env.NODE_ENV === 'production';
 // const base = isGitHubPages ? '/two-brothers/' : '/';
-const base = '/two-brothers/' ;
+const base = '/two-brothers/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
