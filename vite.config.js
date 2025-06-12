ef67ce60239cec3e35712c065c4e1213fed8b6f3
John Clayton <john@effective-flow.ch>
2025-06-12 06:50:52 +0200

    Check config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-﻿import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+﻿import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+const isGitHubPages = process.env.NODE_ENV === 'production';
+const base = isGitHubPages ? '/two-brothers/' : '/';
+
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/two-brothers'
+    base : base,
 });
